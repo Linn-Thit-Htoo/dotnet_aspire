2cb9675cd056f856646674af7e7c429cd080de47
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-06 13:28:16 +0630

    -

--- dotnet_aspire.BlogMicroservice/Controllers/BlogController.cs
+++ dotnet_aspire.BlogMicroservice/Controllers/BlogController.cs
@@ -10,40 +10,42 @@ public class BlogController : ControllerBase
     [HttpGet("GetBlogs")]
     public IActionResult GetBlogs()
     {
-        return Ok(new List<BlogModel>()
-        {
-            new BlogModel
+        return Ok(
+            new List<BlogModel>()
             {
-                BlogId = 1,
-                BlogTitle = "Blog Title 1",
-                BlogAuthor = "Blog Author 1",
-                BlogContent = "Blog Content 1",
-                IsDeleted = false
-            },
-            new BlogModel
-            {
-                BlogId = 2,
-                BlogTitle = "Blog Title 2",
-                BlogAuthor = "Blog Author 2",
-                BlogContent = "Blog Content 2",
-                IsDeleted = false
-            },
-            new BlogModel
-            {
-                BlogId = 3,
-                BlogTitle = "Blog Title 3",
-                BlogAuthor = "Blog Author 3",
-                BlogContent = "Blog Content 3",
-                IsDeleted = false
-            },
-            new BlogModel
-            {
-                BlogId = 4,
-                BlogTitle = "Blog Title 4",
-                BlogAuthor = "Blog Author 4",
-                BlogContent = "Blog Content 4",
-                IsDeleted = false
-            },
-        });
+                new BlogModel
+                {
+                    BlogId = 1,
+                    BlogTitle = "Blog Title 1",
+                    BlogAuthor = "Blog Author 1",
+                    BlogContent = "Blog Content 1",
+                    IsDeleted = false,
+                },
+                new BlogModel
+                {
+                    BlogId = 2,
+                    BlogTitle = "Blog Title 2",
+                    BlogAuthor = "Blog Author 2",
+                    BlogContent = "Blog Content 2",
+                    IsDeleted = false,
+                },
+                new BlogModel
+                {
+                    BlogId = 3,
+                    BlogTitle = "Blog Title 3",
+                    BlogAuthor = "Blog Author 3",
+                    BlogContent = "Blog Content 3",
+                    IsDeleted = false,
+                },
+                new BlogModel
+                {
+                    BlogId = 4,
+                    BlogTitle = "Blog Title 4",
+                    BlogAuthor = "Blog Author 4",
+                    BlogContent = "Blog Content 4",
+                    IsDeleted = false,
+                },
+            }
+        );
     }
 }
