c835cebddeba27fd2592d2db073d0e5dfcc5b6a1
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-06 13:29:24 +0630

    -

--- dotnet_aspire.ProductMicroservice/Controllers/ProductController.cs
+++ dotnet_aspire.ProductMicroservice/Controllers/ProductController.cs
@@ -1,36 +1,35 @@
 ﻿using dotnet_aspire.ProductMicroservice.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace dotnet_aspire.ProductMicroservice.Controllers
+namespace dotnet_aspire.ProductMicroservice.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductController : ControllerBase
+    [HttpGet("GetProducts")]
+    public IActionResult GetProducts()
     {
-        [HttpGet("GetProducts")]
-        public IActionResult GetProducts()
+        return Ok(new List<ProductModel>()
         {
-            return Ok(new List<ProductModel>()
+            new ProductModel
             {
-                new ProductModel
-                {
-                    ProductId = 1,
-                    ProductName = "Product 1",
-                    Price = 1000
-                },
-                new ProductModel
-                {
-                    ProductId = 2,
-                    ProductName = "Product 2",
-                    Price = 1000
-                },
-                new ProductModel
-                {
-                    ProductId = 3,
-                    ProductName = "Product 3",
-                    Price = 1000
-                }
-            });
-        }
+                ProductId = 1,
+                ProductName = "Product 1",
+                Price = 1000
+            },
+            new ProductModel
+            {
+                ProductId = 2,
+                ProductName = "Product 2",
+                Price = 1000
+            },
+            new ProductModel
+            {
+                ProductId = 3,
+                ProductName = "Product 3",
+                Price = 1000
+            }
+        });
     }
 }
