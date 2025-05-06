67a1e228b303ea1acc8aad1f1feee91bc283083d
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-06 13:28:23 +0630

    -

--- dotnet_aspire.BlogMicroservice/Models/BlogModel.cs
+++ dotnet_aspire.BlogMicroservice/Models/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace dotnet_aspire.BlogMicroservice.Models
+﻿namespace dotnet_aspire.BlogMicroservice.Models;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsDeleted { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsDeleted { get; set; }
 }
